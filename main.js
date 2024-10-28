0d839d3f86d04b7578c116b275c502716cb5233b
Hai-Ho-1990 <juno.hu2090@gmail.com>
2024-10-28 14:21:12 +0100

    Applying Search function

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const pokemonApi = 'https://pokeapi.co/api/v2/pokemon?limit=151';
 // Hämtar DOM
 let containerPokemon = document.querySelector('.pokemon-container');
 let loadMore = document.querySelector('.btn-danger');
+let filteredPokemon = [];
 
 // Fetch Url och hämta url för 151 pokemon i json objekt
 
@@ -87,15 +88,9 @@ async function displayPokemon() {
         loadMore.style.display = 'none';
     }
 }
-
-//Varje gång button trycks ska en sidan med 12 pokemon visas.
-loadMore.addEventListener('click', () => {
-    currentPage++;
-
-    displayPokemon();
-});
-
 displayPokemon();
+
+//------------------------------------------------------------------------------
 
 //Ändra & spara header & footer bakgrund färg när användare bockar av checkbox
 let checkbox = document.querySelector('#red-mode-toggle');
@@ -130,6 +125,7 @@ function changeStyle() {
         (header.style = 'background:white'),
             (heading.style = 'color: black'),
             (navCollapse.style = 'background: white !important'),
+            (copyright.style = 'color: black'),
             (footer.style = 'background: white');
         for (let i = 0; i < navLink.length; i++) {
             navLink[i].style = 'color:black !important';
@@ -155,3 +151,82 @@ if (saveCheckboxStatus) {
     checkbox.checked = JSON.parse(saveCheckboxStatus);
     changeStyle();
 }
+
+//------------------------------------------------------------------------------
+/* Pokemon sökning */
+let message = document.querySelector('.message');
+let inputElement = document.getElementById('search');
+let pokedexText = document.querySelector('main h1');
+
+inputElement.addEventListener('input', function (event) {
+    let input = event.target.value.toLowerCase(); //
+
+    filteredPokemon = allPokemon.filter((pokemon) =>
+        pokemon.name.toLowerCase().includes(input)
+    );
+    containerPokemon.innerHTML = '';
+
+    //Om det finns inga pokemon att visa ska fel meddelande visas upp.
+    if (filteredPokemon.length === 0) {
+        message.style.display = 'block';
+        loadMore.style.display = 'none';
+        pokedexText.style.display = 'none';
+    } else {
+        message.style.display = 'none';
+        displayFilteredPokemon();
+    }
+    //Om användaren rensar sökning ska alla pokemon visas igen.
+    if (inputElement.value === 0) {
+        displayPokemon();
+        loadMore.style.display = 'block';
+    }
+});
+
+//Skapa en funktion för att visa filtrerad pokemon
+function displayFilteredPokemon() {
+    //start index
+    let start = (currentPage - 1) * pokemonPerPage;
+    // Sista index
+    let end = currentPage * pokemonPerPage;
+    //Visa endast 12 pokemon på sidan
+    let pokemonToDisplay = filteredPokemon.slice(start, end);
+
+    if (end >= filteredPokemon.length) {
+        loadMore.style.display = 'none';
+    } else {
+        loadMore.style.display = 'block';
+    }
+
+    // Vi har en lista av alla pokemon objekter. Loopa den listan för att ta ut varje pokemon och dess egenskaper */
+    pokemonToDisplay.forEach((pokemon) => {
+        let imgSrc = pokemon.sprites.other['official-artwork'].front_default;
+        let pokemonName = pokemon.name;
+        let order = pokemon.id;
+        //Skapar en card för varje pokemon och visa upp den på webbläsare
+        containerPokemon.innerHTML += `<div class="col-md-4"> <div class="card" >
+    <img src="${imgSrc}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h6 class="card-order"> #${order}</h6>
+      <h5 class="card-title"> ${pokemonName}</h5>
+    </div>
+  </div></div>`;
+    });
+}
+
+//Varje gång button trycks ska en sidan med 12 pokemon visas.
+loadMore.addEventListener('click', () => {
+    currentPage++;
+    // Om sökfältet är tom så visas alla pokemon annars endast filtrerad pokemon visas.
+    if (inputElement.value.trim() === '') {
+        displayPokemon();
+    } else {
+        displayFilteredPokemon();
+    }
+});
+
+//------------------------------------------------------------------------------
+
+// När man klickar på en specifik pokemon så hänvisar det till en annan html sidan.
+pokedexContainer.addEventListener('click', function () {
+    window.location.href = 'detail.html';
+});
